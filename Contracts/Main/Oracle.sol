a4fa0df05df850765a53a8f2fa34a7e2e4ed09dd
RobertMCForster <rforster510@yahoo.com>
2017-11-21 13:26:37 -0600

    Heavily Modified to Query for All Cryptos at Once

--- Contracts/Main/Oracle.sol
+++ Contracts/Main/Oracle.sol
@@ -2,24 +2,25 @@ pragma solidity ^0.4.17;
 import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
 import "github.com/Arachnid/solidity-stringutils/strings.sol";
 import "./Investment.sol";
-import "./Privileged.sol";
 
 /**
  * @title Oracle
  * @dev This contract queries cryptocompare every 60 seconds
- * @dev to find the most recent prices for all investable cryptos.
+ * @dev to find the most recent prices for all investible cryptos.
 */
 
-contract Oracle is usingOraclize, Privileged {
+contract Oracle is usingOraclize {
     using strings for *;
     
-    uint256 public thisPrice;
+    // Cryptocompare API URL
+    string url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP,LTC,DASH,BCH,XMR,XEM,EOS&tsyms=USD";
+
+    // These are used to split a raw string to determine prices of each asset.
+    string[] separators = ['{"BTC":{"USD":','},"ETH":{"USD":','},"XRP":{"USD":','},"LTC":{"USD":','},"DASH":{"USD":','},"BCH":{"USD":','},"XMR":{"USD":','},"XEM":{"USD":','},"EOS":{"USD":','}}'];
+
     Investment investContract;
-    mapping (string => string) symbolUrls; // Symbol ("COIN") of the crypto to search and URL to search
-    mapping (string => uint256) symbolIds; // Find the crypto ID of the given symbol string
-    
+
     event newOraclizeQuery(string description);
-    event newPrice(string symbol, uint256 price);
 
 /** ****************************** Defaults ************************************* **/    
 
@@ -36,81 +37,43 @@ contract Oracle is usingOraclize, Privileged {
     **/
     function __callback(bytes32 myid, string result, bytes proof) {
         if (msg.sender != oraclize_cbAddress()) throw;
-        
-        string memory symbol = findSymbol(result);
-        uint256 price = findPrice(result);
-        newPrice(symbol, price);
-        thisPrice = price;
     
-        investContract.setPrice(symbolIds[symbol], price);
-        update(symbol);
+        uint256[9] memory prices = findPrices(result);
+        
+        investContract.setPrices(prices);
+        update();
     }
     
-    function update(string _symbol) payable {
-        string _url = symbolUrls[_symbol];
+    function update() payable {
         if (oraclize.getPrice("URL") > this.balance) {
             newOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
         } else {
             newOraclizeQuery("Oraclize query was sent, standing by for the answer..");
-            oraclize_query(60, "URL", _url);
+            oraclize_query(60, "URL", url, 500000);
         }
     }
     
-/** ****************************** Only Owner *********************************** **/
+/** ******************************** Internal *********************************** **/
 
     /**
-     * @dev Owner can add a new crypto to the update list.
-     * @dev In the future this can also add crypto to Investment contract.
-     * @param _symbol The symbol (i.e. COIN) to be added to the list (all caps).
-     * @param _url The URL that should be scraped to find the price (encased in json()).
+     * @dev Cycles through a list of separators to split the api
+     * @dev result string. Returns list so that we can update invest contract with values.
+     * @param _result The raw string returned from the cryptocompare api with all crypto prices.
     **/
-    function updateUrls(string _symbol, string _url, uint256 _cryptoId)
-      external
-      onlyOwner
-    returns (bool success)
-    {
-        symbolUrls[_symbol] = _url;
-        symbolIds[_symbol] = _cryptoId;
-        return true;
-    }
-    
-/** ******************************** Internal *********************************** **/
-    
-    /**
-     * @dev Searches result string to find which crypto is being returned.
-     * @param _result The string result given back by Oraclize.
-    **/
-    function findSymbol(string _result)
+    function findPrices(string _result) 
       internal
-    returns (string symbol)
+    returns (uint256[9] finals)
     {
-        var s = _result.toSlice();
-        strings.slice memory part;
-        s.rsplit('"'.toSlice(), part);
-        s.rsplit('"'.toSlice(), part);
-        s.rsplit('"'.toSlice(), part);
-        s.split('"'.toSlice(), part);
-        var sString = s.toString();
-        return sString;
+        for(uint256 i = 0; i < separators.length - 1; i++) {
+            var s = _result.toSlice();
+            strings.slice memory part;
+            s.split(separators[i].toSlice(), part);
+            s.rsplit(separators[i + 1].toSlice(), part);
+            
+            var sString = s.toString();
+            uint256 newPrice = parseInt(sString, 18);
+            finals[i] = newPrice;
+        }
     }
-    
-    /**
-     * @dev Searches result string to find new price of target crypto.
-     * @param _result The string result given back by Oraclize.
-    **/
-    function findPrice(string _result)
-      internal
-    returns (uint256 newPrice)
-    {
-        var s = _result.toSlice();
-        strings.slice memory part;
-        s.split(':'.toSlice(), part);
-        s.split(':'.toSlice(), part);
-        s.rsplit('}'.toSlice(), part);
-    
-        var sString = s.toString();
-        newPrice = parseInt(sString, 18);
-        return newPrice;
-    }
- 
+
 } 
