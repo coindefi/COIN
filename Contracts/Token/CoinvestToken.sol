f6cd0715a14ce3f159dceedfbbce5bfb1ff5a54b
Dexaran <dexaran820@gmail.com>
2017-12-25 07:28:08 +0000

    Update CoinvestToken.sol

--- Contracts/Token/CoinvestToken.sol
+++ Contracts/Token/CoinvestToken.sol
@@ -1,26 +1,58 @@
-pragma solidity ^0.4.18;
-/**
- * @title Coinvest Token
- * @dev Just a very straightforward ERC20 contract.
-**/
+pragma solidity ^0.4.9;
 
-contract CoinvestToken {
-    string public constant symbol = "COIN";
-    string public constant name = "Coinvest";
-    
-    // Storing small numbers is cheaper.
-    uint256 public constant decimals = 18;
-    uint256 _totalSupply = 1000000 * (10 ** 18);
+ /*
+ * Contract that is working with ERC223 tokens
+ */
+ 
+contract ContractReceiver {
+    function tokenFallback(address _from, uint _value, bytes _data);
+} 
 
-    // Balances for each account
-    mapping(address => uint256) balances;
+ /**
+ * ERC23 token by Dexaran
+ *
+ * https://github.com/Dexaran/ERC23-tokens
+ */
+ 
+ 
+ /* https://github.com/LykkeCity/EthereumApiDotNetCore/blob/master/src/ContractBuilder/contracts/token/SafeMath.sol */
+contract SafeMath {
+    uint256 constant public MAX_UINT256 =
+    0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+
+    function safeAdd(uint256 x, uint256 y) constant internal returns (uint256 z) {
+        if (x > MAX_UINT256 - y) throw;
+        return x + y;
+    }
+
+    function safeSub(uint256 x, uint256 y) constant internal returns (uint256 z) {
+        if (x < y) throw;
+        return x - y;
+    }
+
+    function safeMul(uint256 x, uint256 y) constant internal returns (uint256 z) {
+        if (y == 0) return 0;
+        if (x > MAX_UINT256 / y) throw;
+        return x * y;
+    }
+}
+ 
+contract CoinvestToken is SafeMath {
+    event Transfer(address indexed from, address indexed to, uint value, bytes data);
+    event Approval(address indexed _from, address indexed _spender, uint indexed _amount);
+
+    mapping(address => uint) balances;
 
     // Owner of account approves the transfer of an amount to another account
     mapping(address => mapping (address => uint256)) allowed;
+  
 
-    event Transfer(address indexed _from, address indexed _to, uint indexed _amount);
-    event Approval(address indexed _from, address indexed _spender, uint indexed _amount);
-
+    string public constant symbol = "COIN";
+    string public constant name = "Coinvest";
+    
+    uint256 public constant decimals = 18;
+    uint256 _totalSupply = 1000000 * (10 ** 18);
+    
     /**
      * @dev Set owner and beginning balance.
     **/
@@ -29,47 +61,88 @@ contract CoinvestToken {
     {
         balances[msg.sender] = _totalSupply;
     }
-
-    /**
-     * @dev Return total supply of token
-    **/
-    function totalSupply() 
-      external
-      constant 
-     returns (uint256) 
-    {
-        return _totalSupply;
+  
+  
+    // Function that is called when a user or another contract wants to transfer funds .
+    function transfer(address _to, uint _value, bytes _data, string _custom_fallback) returns (bool success) {
+      
+        if(isContract(_to)) {
+            if (balanceOf(msg.sender) < _value) throw;
+            balances[msg.sender] = safeSub(balanceOf(msg.sender), _value);
+            balances[_to] = safeAdd(balanceOf(_to), _value);
+            assert(_to.call.value(0)(bytes4(sha3(_custom_fallback)), msg.sender, _value, _data));
+            Transfer(msg.sender, _to, _value, _data);
+            return true;
+        }
+        else {
+            return transferToAddress(_to, _value, _data);
+        }
     }
 
-    /**
-     * @dev Return balance of a certain address.
-     * @param _owner The address whose balance we want to check.
-    **/
-    function balanceOf(address _owner)
-      external
-      constant 
-    returns (uint256) 
-    {
-        return balances[_owner];
+    function ERC20transfer(address _to, uint _value, bytes _data) returns (bool success) {
+        bytes memory empty;
+        return transferToAddress(_to, _value, empty);
+    }
+  
+
+    // Function that is called when a user or another contract wants to transfer funds .
+    function transfer(address _to, uint _value, bytes _data) returns (bool success) {
+        if(isContract(_to)) {
+            return transferToContract(_to, _value, _data);
+        }
+        else {
+            return transferToAddress(_to, _value, _data);
+        }
+    }
+  
+    // Standard function transfer similar to ERC20 transfer with no _data .
+    // Added due to backwards compatibility reasons .
+    function transfer(address _to, uint _value) returns (bool success) {
+      
+        //standard function transfer similar to ERC20 transfer with no _data
+        //added due to backwards compatibility reasons
+        bytes memory empty;
+        if(isContract(_to)) {
+            return transferToContract(_to, _value, empty);
+        }
+        else {
+            return transferToAddress(_to, _value, empty);
+        }
     }
 
-    /**
-     * @dev Transfers coins from one address to another.
-     * @param _to The recipient of the transfer amount.
-     * @param _amount The amount of tokens to transfer.
-    **/
-    function transfer(address _to, uint256 _amount) 
-      external
-    returns (bool success)
-    {
-        // Throw if insufficient balance
-        require(balances[msg.sender] >= _amount);
-        
-        balances[msg.sender] -= _amount;
-        balances[_to] += _amount;
+    //assemble the given address bytecode. If bytecode exists then the _addr is a contract.
+    function isContract(address _addr) private returns (bool is_contract) {
+        uint length;
+        assembly {
+            //retrieve the size of the code on target address, this needs assembly
+            length := extcodesize(_addr)
+        }
+        return (length>0);
+    }
 
-        Transfer(msg.sender, _to, _amount);
+    //function that is called when transaction target is an address
+    function transferToAddress(address _to, uint _value, bytes _data) private returns (bool success) {
+        if (balanceOf(msg.sender) < _value) throw;
+        balances[msg.sender] = safeSub(balanceOf(msg.sender), _value);
+        balances[_to] = safeAdd(balanceOf(_to), _value);
+        Transfer(msg.sender, _to, _value, _data);
         return true;
+    }
+  
+    //function that is called when transaction target is a contract
+    function transferToContract(address _to, uint _value, bytes _data) private returns (bool success) {
+        if (balanceOf(msg.sender) < _value) throw;
+        balances[msg.sender] = safeSub(balanceOf(msg.sender), _value);
+        balances[_to] = safeAdd(balanceOf(_to), _value);
+        ContractReceiver receiver = ContractReceiver(_to);
+        receiver.tokenFallback(msg.sender, _value, _data);
+        Transfer(msg.sender, _to, _value, _data);
+        return true;
+    }
+
+
+    function balanceOf(address _owner) constant returns (uint balance) {
+        return balances[_owner];
     }
 
     /**
