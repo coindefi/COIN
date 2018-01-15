07dd5373ef48946a0cd1b3188f2c0077569ce658
Dexaran <dexaran820@gmail.com>
2018-01-15 09:36:00 +0000

    Update ICO.sol

--- Contracts/Main/ICO.sol
+++ Contracts/Main/ICO.sol
@@ -4,6 +4,7 @@ import './SafeMath.sol';
 
 contract CoinvestToken {
     function transfer(address _to, uint256 _amount);
+    function balanceOf(address _owner) constant returns (uint balance);
 }
 
 contract ICO {
@@ -14,7 +15,14 @@ contract ICO {
     address public token_address; // << Replace with token contract address
     CoinvestToken token = CoinvestToken(token_address);
     
-    uint256 public price = 100; // Amount of tokens to be sent per each WEI(ETH) contributed.
+    uint256 public constant price = 2058; // Amount of tokens to be sent per each WEI(ETH) contributed.
+    /**
+     * NOTE: The `price` should be calculated as follows:
+     * Targeted parameters 1100 COIN for $700 USD
+     * 
+     * Assume ETH price = $1310 USD
+     * 1310 / 700 * 1100 = 2058 
+     */
     
     uint256 public start_block = block.number;
     uint256 public end_block = block.number + 172800;
@@ -36,14 +44,14 @@ contract ICO {
         end_block = _end_block;
     }
     
-    function adjust_price(uint256 _price) only_owner
-    {
-        price = _price;
-    }
-    
     function withdraw() only_owner
     {
         owner.send(this.balance);
+    }
+    
+    function withdraw_token() only_owner
+    {
+        token.transfer(msg.sender, token.balanceOf(this));
     }
     
     
